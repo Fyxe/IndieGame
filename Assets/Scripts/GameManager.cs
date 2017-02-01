146ccc7a5d7bae782cc35d133bf87a783876f062
Trevor <trevor96@tamu.edu>
2017-02-01 08:50:52 -0600

    Created Game Manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class placeholder : MonoBehaviour {
+public class GameManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
