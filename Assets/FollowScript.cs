1e5e9be44fcefb46f5d126d527b352d580d0a3b9
Trevor <trevor96@tamu.edu>
2017-02-02 23:06:10 -0600

    -Created desk like area -added keyboard -created basic movement controls -created basic camera follow script (changing soon) -added tan to basic colors

---
+++ Assets/FollowScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+	Class Purpose: Follow a given gameobject and watch it from a given offset
+*/
+public class FollowScript : MonoBehaviour {
+
+	//===============================================[Variables]====================================================
+
+	public Vector3 Offset = new Vector3(0,8,-3);
+
+	public float LerpSpeed = 0.07f;
+
+	public GameObject Followee = null;
+
+	//============================================[Unity Functions]=================================================
+
+	void FixedUpdate(){
+		if (Followee != null) {
+			transform.LookAt (Followee.transform);
+			transform.position = Vector3.Slerp (transform.position,Followee.transform.position + Offset,LerpSpeed);
+		}
+	}
+
+	//===============================================[Functions]====================================================
+
+	//==============================================================================================================	
+
+	//--------------------------------------------------------------------------------------------------------------
+
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -34,6 +34,9 @@ public class Player : MonoBehaviour {
 
 
 	[Header("UnityVariables")]
+	Rigidbody rb;
+	float Jump_Delay 				= 0.1f;
+	float Jump_Next 				= 0;
 	bool Cor_SlowUpdate_Helper 		= false;							
 	float SlowUpdateTime 			= 0.1f;							
 	Coroutine Alpha;										
@@ -41,7 +44,9 @@ public class Player : MonoBehaviour {
 	//============================================[Unity Functions]=================================================
 	#region UnityFunctions
 
-	//void Awake(){}
+	void Awake(){
+		rb = GetComponent<Rigidbody> ();
+	}
 
 	//--------------------------------------------------------------------------------------------------------------
 
@@ -54,15 +59,71 @@ public class Player : MonoBehaviour {
 	//--------------------------------------------------------------------------------------------------------------
 
 	void Update(){
-		Input ();
+		User_Input ();
 	}
 
 	#endregion
 	//===============================================[Functions]====================================================
 	#region Functions
 
-	void Input(){
+	void User_Input(){
+		Input_Movement();
+		Input_Combat  ();
+	}
+
+	//--------------------------------------------------------------------------------------------------------------
+
+	void Input_Movement (){
+		float Damper = 0.1f;
+		float xx = Input.GetAxis ("Horizontal") * Get_MoveSpeed() * Damper;
+		float zz = Input.GetAxis ("Vertical")   * Get_MoveSpeed() * Damper;
+
+		Vector3 Velo = rb.velocity;
+
+		if (IsGrounded) {
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				Jump ();
+				Debug.Log ("J");
+			}
+		} else {	// If not IsGrounded
+			xx *= 0.5f;
+			zz *= 0.5f;
+		}
+
+		Velo.x = xx;
+		Velo.z = zz;
+
+		rb.velocity = Velo;
+	}
+
+	//--------------------------------------------------------------------------------------------------------------
+
+	void Jump(){
+		if (Time.time > Jump_Next) {
+			Jump_Next = Time.time + Jump_Delay;
+			rb.AddForce (0f, Jump_Force, 0f);
+		}
+	}
+
+	//--------------------------------------------------------------------------------------------------------------
+
+	void Input_Combat(){
 		
+	}
+
+	//--------------------------------------------------------------------------------------------------------------
+
+	float Get_MoveSpeed(){
+		switch (Move_Mode) {
+		case 0:
+			return Move_Speed * 0.65f;
+		case 1:
+			return Move_Speed;
+		case 2:
+			return Move_Speed * 1.7f;
+		default:
+			return Move_Speed * 0.1f;
+		}
 	}
 
 	//--------------------------------------------------------------------------------------------------------------
