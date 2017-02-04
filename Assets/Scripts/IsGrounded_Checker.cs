f88704eb19f8dc43ca7a4221e00fe2dd09c208b0
Trevor <trevor96@tamu.edu>
2017-02-04 10:11:20 -0600

    -Added extra raycasts to ground checker. -Created system that allows you to set the number of extra raycasts and adjusts accordingly.

--- Assets/Scripts/IsGrounded_Checker.cs
+++ Assets/Scripts/IsGrounded_Checker.cs
@@ -7,10 +7,13 @@ public class IsGrounded_Checker : MonoBehaviour {
 	RaycastHit h;
 	Ray r;
 
+	int NumberOfExtraRays = 8;
+	float Radius = 0.25f;
 	bool IsGrounded = false;
 
 	void Update(){
 		r = new Ray (transform.position + Vector3.up * 0.1f,Vector3.down);
+		IsGrounded = false;
 
 		if (Physics.Raycast (r, out h)) {
 			if (h.distance < 0.11f) {
@@ -18,12 +21,35 @@ public class IsGrounded_Checker : MonoBehaviour {
 				IsGrounded = true;
 			} else {
 				Debug.DrawRay (transform.position,r.direction,Color.red);
-				IsGrounded = false;
+				//IsGrounded = false;
 			}
 		} else {
 			Debug.DrawRay (transform.position,r.direction,Color.blue);
-			IsGrounded = false;
+			//IsGrounded = false;
 		}
+
+		for (int i = 0; i < NumberOfExtraRays; i++) {
+			float x = transform.position.x + Radius * Mathf.Cos (2 * Mathf.PI * i / NumberOfExtraRays);
+			float z = transform.position.z + Radius * Mathf.Sin (2 * Mathf.PI * i / NumberOfExtraRays);
+			Vector3 NewPos = new Vector3 (x, transform.position.y + 0.2f, z);
+
+			r = new Ray (NewPos,Vector3.down);
+
+			if (Physics.Raycast (r, out h)) {
+				if (h.distance < 0.22f) {
+					Debug.DrawRay (NewPos,r.direction,Color.green);
+					IsGrounded = true;
+				} else {
+					Debug.DrawRay (NewPos,r.direction,Color.red);
+					//IsGrounded = false;
+				}
+			} else {
+				Debug.DrawRay (NewPos,r.direction,Color.blue);
+				//IsGrounded = false;
+			}
+
+		}
+
 
 		transform.root.GetComponent<Player> ().IsGrounded = IsGrounded;
 	}
